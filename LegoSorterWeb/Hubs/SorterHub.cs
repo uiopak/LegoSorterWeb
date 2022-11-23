a18bc69bd81bb861241a1ac369a851a0ef849524
uiopak <44174807+uiopak@users.noreply.github.com>
2022-11-23 23:40:26 +0100

    Fix .NET 7 problem

--- LegoSorterWeb/Hubs/SorterHub.cs
+++ LegoSorterWeb/Hubs/SorterHub.cs
@@ -5,7 +5,7 @@ namespace LegoSorterWeb.Hubs
 {
     public class SorterHub : Hub
     {
-        public async Task SendMessage(IEnumerable<Message> messages)
+        public async Task SendMessage(List<Message> messages)
         //public async Task SendMessage(int ymin, int xmin, int ymax, int xmax, string label, float score)
         {
             await Clients.All.SendAsync("messageReceived", messages);
